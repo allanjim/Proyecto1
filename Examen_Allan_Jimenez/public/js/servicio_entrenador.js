b773a0ea72d463afa414c5986d6e7d6a6e02a7ea
Allan Jimenez <ajimenezr1@ucenfotec.ac.cr>
2018-07-21 15:47:05 -0600

    Revert "Cambio en el front end y interfaz de las páginas"
    This reverts commit fee5c8f60ad6ffd9907ca1ffa8cb611b2697132c.

--- Examen_Allan_Jimenez/public/js/servicio_entrenador.js
+++ Examen_Allan_Jimenez/public/js/servicio_entrenador.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 'use strict';
 
 
@@ -54,4 +55,62 @@ function obtenerListaEntrenador(){
       return respuesta;
     
     return listaEntrenador;
+=======
+'use strict';
+
+
+function registrarEntrenador(plistaEntrenador){
+    let respuesta = '';
+    let peticion = $.ajax({
+        url : 'http://localhost:4000/api/registrar_entrenador',
+        type : 'post',
+        contentType : 'application/x-www-form-urlencoded; charset=utf-8',
+        dataType : 'json',
+        async : false,
+        data:{
+            nombre_Entrenador : plistaEntrenador[0],
+            numero_Entrenador : plistaEntrenador[1],
+            edad_Entrenador : plistaEntrenador[2],
+            sexo_Entrenador : plistaEntrenador[3],
+            foto : plistaEntrenador [4]
+        }
+      });
+    
+      peticion.done(function(response){
+       respuesta = response;
+      });
+    
+      peticion.fail(function(response){
+       
+      });
+
+      return respuesta;
+}
+function obtenerListaEntrenador(){
+    let listaEntrenador = [];
+
+    let respuesta = '';
+    let peticion = $.ajax({
+        url : 'http://localhost:4000/api/listar_entrenador',
+        type : 'get',
+        contentType : 'application/x-www-form-urlencoded; charset=utf-8',
+        dataType : 'json',
+        async : false,
+        data:{
+            
+        }
+      });
+    
+      peticion.done(function(response){
+       respuesta = response;
+      });
+    
+      peticion.fail(function(response){
+       
+      });
+
+      return respuesta;
+    
+    return listaEntrenador;
+>>>>>>> parent of fee5c8f... Cambio en el front end y interfaz de las páginas
 }
--- Examen_Allan_Jimenez/public/js/servicio_pokemon.js
+++ Examen_Allan_Jimenez/public/js/servicio_pokemon.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 'use strict';
 
 
@@ -54,4 +55,69 @@ function obtenerListaPokemon(){
       return respuesta;
     
     return listaPokemon;
+=======
+/*
+Responsabilidades del servicio
+    - Procesamiento de datos (cálculos)
+    - Almacenamiento temporal de los datos
+    - Comunicar el public (front-end) con el api (back-end)
+*/
+
+'use strict';
+
+
+function registrarPokemon(plistaPokemon){
+    let respuesta = '';
+    let peticion = $.ajax({
+        url : 'http://localhost:4000/api/registrar_pokemon',
+        type : 'post',
+        contentType : 'application/x-www-form-urlencoded; charset=utf-8',
+        dataType : 'json',
+        async : false,
+        data:{
+            nombre_pokemon : plistaPokemon[0],
+            numero_pokemon : plistaPokemon[1],
+            tipo_pokemon : plistaPokemon[2],
+            tipoSecundario_pokemon : plistaPokemon[3],
+            foto : plistaPokemon [4]
+        }
+      });
+    
+      peticion.done(function(response){
+       respuesta = response;
+      });
+    
+      peticion.fail(function(response){
+       
+      });
+
+      return respuesta;
+}
+function obtenerListaPokemon(){
+    let listaPokemon = [];
+
+    let respuesta = '';
+    let peticion = $.ajax({
+        url : 'http://localhost:4000/api/listar_pokemon',
+        type : 'get',
+        contentType : 'application/x-www-form-urlencoded; charset=utf-8',
+        dataType : 'json',
+        async : false,
+        data:{
+            
+        }
+      });
+    
+      peticion.done(function(response){
+       respuesta = response;
+      });
+    
+      peticion.fail(function(response){
+       
+      });
+
+      return respuesta;
+    
+    return listaPokemon;
+>>>>>>> parent of fee5c8f... Cambio en el front end y interfaz de las páginas
 }
